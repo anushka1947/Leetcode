b286901747b715c5c38a10fc3ec374789ed3fc05
Anushka Chaturvedi <anushka.chaturvedi.200514@gmail.com>
2025-04-04 12:39:44 +0530

    Time: 2 ms (69.54%), Space: 44.1 MB (98.01%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+    
+        int minCost = prices[0] + prices[1];
+        if(minCost <= money ){
+            return money - minCost ;
+        }
+        else{
+            return money;
+        }
+
+
+    }
+}
